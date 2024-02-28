68ece6d12588cfc09483bf6d8992c6a92f0e0394
David Ziegler <dz@entropy.org>
2024-02-28 09:02:40 -0500

    Added support for dictionary content + added image tagger + general import improvements

---
+++ tagger/import-mmo.ts
@@ -0,0 +1,313 @@
+/**
+ * Importer for the MMO legacy format.
+ *
+ */
+import * as fs from "https://deno.land/std@0.195.0/fs/mod.ts";
+import * as utils from "../utils/utils.ts";
+import {unwrap} from "../utils/utils.ts";
+import { db, Db, PreparedQuery, assertDmlContainsAllFields, boolnum, defaultDbPath } from "./db.ts";
+import {block} from "../utils/strings.ts";
+import { ScannedDocument, ScannedDocumentOpt, selectScannedDocument, ScannedPage, ScannedPageOpt, Assertion, assertionFieldNames } from './schema.ts';
+import * as config from "./config.ts";
+
+// TODO: CLI, read the json in.
+// TODO: recursively build structure
+
+async function importMMO() {
+    // XXX TODO move this file/path into our import tree.
+    const entries = JSON.parse(await Deno.readTextFile("/home/dziegler/wordwiki/importer/mikmaq/entry-tuples.json")) as Entry[];
+
+    console.info('entry count', entries.length);
+
+    console.info(entries.map(e=>e.entry_id).toSorted());
+
+    db().beginTransaction();
+    db().execute('DELETE FROM assertion', {});
+
+    entries.forEach(e=>true ? importEntry(e) : undefined);
+    
+    db().endTransaction();
+}
+
+function insertAssertion(assertion: Assertion): Assertion {
+    //console.info('inserting assertion', assertion);
+    db().execute<Assertion>(
+        `INSERT INTO assertion (${assertionFieldNames.join(',')}) VALUES (${assertionFieldNames.map(f=>':'+f).join(',')}) RETURNING assertion_id AS id`, assertion);
+    return assertion;
+}
+
+function createAssertion(parent: Assertion|undefined, depth: number,
+                         id: number, ty: string, fields: Partial<Assertion>) {
+    return Object.assign({
+        assertion_id: id,
+        parent_id: parent?.id,
+        id: id,
+        ty: ty,
+        ty1: parent?.ty1,
+        id1: parent?.id1,
+        ty2: parent?.ty2,
+        id2: parent?.id2,
+        ty3: parent?.ty3,
+        id3: parent?.id3,
+        ty4: parent?.ty4,
+        id4: parent?.id4,
+        ty5: parent?.ty5,
+        id5: parent?.id5,
+        [`id${depth}`]: id,
+        [`ty${depth}`]: ty,
+    }, fields);
+}
+
+interface Entry {
+    entry_id: number;
+    spelling: Spelling[];
+    subentry: Subentry[];
+    internal_note: string;
+    public_note: string;
+}
+
+function importEntry(entry: Entry) {
+    const entryAssertion = insertAssertion(createAssertion(
+        undefined, 1, entry.entry_id, 'lx',
+        {
+            text1: entry.internal_note,
+            text2: entry.public_note,
+        }));
+    entry.spelling.forEach(s=>importSpelling(entryAssertion, s));
+    entry.subentry.forEach(s=>importSubentry(entryAssertion, s));
+}
+
+interface Spelling {
+    spelling_id: number;
+    text: string;
+    variant: string;
+}
+
+function importSpelling(parent: Assertion, spelling: Spelling) {
+    insertAssertion(createAssertion(
+        parent, 2, spelling.spelling_id, 'sp',
+        {
+            text1: spelling.text,
+            locale_expr: spelling.variant,
+        }));
+}
+
+interface Subentry {
+    subentry_id: number;
+    part_of_speech: string;
+    definition: Definition[];
+    gloss: Gloss[];
+    example: Example[];
+    pronunciation_guide: PronunciationGuide[];
+    category: Category[];
+    related_entry: RelatedEntry[];
+    alternate_grammatical_form: AlternateGrammaticalForm[];
+    other_regional_form: OtherRegionalForm[];
+    attr: Attr[];
+    //status: Status[];
+}
+
+function importSubentry(parent: Assertion, subentry: Subentry) {
+    const subentryAssertion = insertAssertion(createAssertion(
+        parent, 2, subentry.subentry_id, 'se',
+        {
+            text1: subentry.part_of_speech,
+        }));
+    subentry.definition.forEach(s=>importDefinition(subentryAssertion, s));
+    subentry.gloss.forEach(s=>importGloss(subentryAssertion, s));
+    subentry.example.forEach(s=>importExample(subentryAssertion, s));
+    subentry.pronunciation_guide.forEach(s=>importPronunciationGuide(subentryAssertion, s));
+    subentry.category.forEach(s=>importCategory(subentryAssertion, s));
+    subentry.related_entry.forEach(s=>importRelatedEntry(subentryAssertion, s));
+    subentry.alternate_grammatical_form.forEach(s=>importAlternateGrammaticalForm(subentryAssertion, s));
+    subentry.other_regional_form.forEach(s=>importOtherRegionalForm(subentryAssertion, s));
+    subentry.attr.forEach(s=>importAttr(subentryAssertion, s));
+    // console.info('STATUS', subentry);
+    // subentry.status.forEach(s=>importStatus(subentryAssertion, s));
+}
+
+interface Definition {
+    definition_id: number;
+    definition: string;
+}
+
+function importDefinition(parent: Assertion, definition: Definition) {
+    insertAssertion(createAssertion(
+        parent, 3, definition.definition_id, 'de',
+        {
+            text1: definition.definition,
+        }));
+}
+
+interface Gloss {
+    gloss_id: number;
+    gloss: string;
+}
+
+function importGloss(parent: Assertion, gloss: Gloss) {
+    insertAssertion(createAssertion(
+        parent, 3, gloss.gloss_id, 'gl',
+        {
+            text1: gloss.gloss,
+        }));
+}
+
+interface Example {
+    example_id: number;
+    translation: string;
+    example_text: ExampleText[];
+}
+
+function importExample(parent: Assertion, example: Example) {
+    const exampleAssertion = insertAssertion(createAssertion(
+        parent, 3, example.example_id, 'ex',
+        {
+            text1: example.translation,
+        }));
+    example.example_text.forEach(s=>importExampleText(exampleAssertion, s));
+}
+
+interface ExampleText {
+    example_text_id: number;
+    text: string;
+    variant: string;
+}
+
+function importExampleText(parent: Assertion, exampleText: ExampleText) {
+    insertAssertion(createAssertion(
+        parent, 4, exampleText.example_text_id, 'extx',
+        {
+            text1: exampleText.text,
+            locale_expr: exampleText.variant,
+        }));
+}
+
+interface PronunciationGuide {
+    pronunciation_guide_id: number;
+    text: string;
+    variant: string;
+}
+
+function importPronunciationGuide(parent: Assertion, pronunciationGuide: PronunciationGuide) {
+    insertAssertion(createAssertion(
+        parent, 3, pronunciationGuide.pronunciation_guide_id, 'pg',
+        {
+            text1: pronunciationGuide.text,
+            locale_expr: pronunciationGuide.variant,
+        }));
+}
+
+interface Category {
+    category_id: number;
+    category: string;
+}
+
+function importCategory(parent: Assertion, category: Category) {
+    insertAssertion(createAssertion(
+        parent, 3, category.category_id, 'ct',
+        {
+            text1: category.category,
+        }));
+}
+
+interface RelatedEntry {
+    related_entry_id: number;
+    unresolved_text: string;
+}
+
+function importRelatedEntry(parent: Assertion, relatedEntry: RelatedEntry) {
+    insertAssertion(createAssertion(
+        parent, 3, relatedEntry.related_entry_id, 're',
+        {
+            text1: relatedEntry.unresolved_text, // ??? ??? TODO WTF ??? XXX ???
+        }));
+}
+
+interface AlternateGrammaticalForm {
+    alternate_grammatical_form_id: number;
+    gloss: string;
+    grammatical_form: string;
+    alternate_form_text: AlternateFormText[];
+}
+
+function importAlternateGrammaticalForm(parent: Assertion, alternateGrammaticalForm: AlternateGrammaticalForm) {
+    const alternateGrammaticalFormAssertion = insertAssertion(createAssertion(
+        parent, 3, alternateGrammaticalForm.alternate_grammatical_form_id, 'ag',
+        {
+            text1: alternateGrammaticalForm.gloss,
+            text2: alternateGrammaticalForm.grammatical_form,
+        }));
+    alternateGrammaticalForm.alternate_form_text.forEach(s=>importAlternateFormText(alternateGrammaticalFormAssertion, s));
+}
+
+interface AlternateFormText {
+    alternate_form_text_id: number;
+    text: string;
+    variant: string;
+}
+
+function importAlternateFormText(parent: Assertion, alternateFormText: AlternateFormText) {
+    insertAssertion(createAssertion(
+        parent, 4, alternateFormText.alternate_form_text_id, 'agtx',
+        {
+            text1: alternateFormText.text,
+            locale_expr: alternateFormText.variant,
+        }));
+}
+
+interface OtherRegionalForm {
+    other_regional_form_id: number;
+    text: string;
+}
+
+function importOtherRegionalForm(parent: Assertion, otherRegionalForm: OtherRegionalForm) {
+    insertAssertion(createAssertion(
+        parent, 3, otherRegionalForm.other_regional_form_id, 'rf',
+        {
+            text1: otherRegionalForm.text,
+        }));
+}
+
+interface Attr {
+    attr_id: number;
+    attr: string;
+    value: string;
+}
+
+function importAttr(parent: Assertion, attr: Attr) {
+    insertAssertion(createAssertion(
+        parent, 3, attr.attr_id, 'at',
+        {
+            text1: attr.attr,
+            text2: attr.value,
+        }));
+}
+
+interface Status {
+    status_id: number;
+    variant: string;
+    status: string;
+    details: string;
+}
+
+function importStatus(parent: Assertion, status: Status) {
+    insertAssertion(createAssertion(
+        parent, 3, status.status_id, 'st',
+        {
+            text1: status.status,
+            text2: status.details,
+            locale_expr: status.variant,
+        }));
+}
+
+async function main(args: string[]) {
+    const [command, ...commandArgs] = args;
+    switch(command) {
+        case 'ImportMMO': await importMMO(); break;
+        default: throw new Error('bad usage');
+    }
+}
+
+if (import.meta.main) {
+    await main(Deno.args);
+}
---
+++ utils/timestamp.ts
@@ -0,0 +1,110 @@
+/**
+ * 2^53-1 == JS Maxint
+ * We allocate 33 bits for the time since local epoch start in seconds ~= 270 years.
+ * We allocate the remaining 20 bits to updates per second ~= 1 million updates per second.
+ * - if we run out of updates in a second, we will just start using the next second.
+ * - as we approach the end of time, we switch to a fixed clock time ...
+ */
+
+
+
+
+// We use a newer epoch start than the default "January 1, 1970,
+// 00:00:00 UTC" so we can go longer before we run out of time bits
+// (and so that future people can adjust the epoch if this happens)
+export const LOCAL_EPOCH_START = +new Date(Date.UTC(2020, 0, 1));
+
+// When we hit the epoch end, the time portion of the timestamp stops
+// moving forward (except as driven by counter overflow).  This will
+// give us 1 billion further events after we hit the end of the epoch.
+export const RAPTURE_TIME = (LOCAL_EPOCH_START + 2**33) - (1024*1024);
+
+// Bottom 20 bits of a timestamp is the counter portion.  (1 million
+// events per second).  (If there is more than 1 million events in a
+// second, we will just advance to the next second).
+const COUNTER_MASK = 0x0FFFFF;
+
+//
+export const END_TIME = Number.MAX_SAFE_INTEGER;
+
+
+/**
+ * Given an existing timestamp, return a higher timestamp.
+ *
+ * If possible, the timestamp will contain the current time.
+ *
+ * The timestamp will not have the correct current time if the lastTimestamp
+ * is in the future (ie. we can only go forward).
+ *
+ * If there are more than one million timestamps requested in one second,
+ * then we will start stealing timestamps from the next second.
+ *
+ * When we get close to the end of days (hundreds of years in the future),
+ * we will stop advancing the time portion of the timestamp, and rely only
+ * on the rollover mechanism.  This will give us an extra billion updates
+ * after the rapture.
+ *
+ * The purpose of this mechanism is to have a smallish always increasing
+ * timestamp that is used to order transactions, while having a (usually
+ * correct) system time embedded in it to make it user intelligible.
+ */
+export function nextTime(lastTimestamp: number): number {
+    const currentSystemTimeInLocalEpoch = currentSystemTimeInLocalEpochOrRaptureTime();
+    const lastTimestampTime = extractTimeFromTimestamp(currentSystemTimeInLocalEpoch);
+    if(currentSystemTimeInLocalEpoch > lastTimestampTime) {
+        return makeTimestamp(currentSystemTimeInLocalEpoch, 0);
+    } else {
+        const nextTimestamp = lastTimestamp+1;
+        if(!Number.isSafeInteger(nextTimestamp))
+            throw new Error('internal error: THE END DAYS ARE HERE!');
+        return nextTimestamp;
+    }
+}
+
+/**
+ * Extract the time portion of a timestamp.
+ */
+export function extractTimeFromTimestamp(timestamp: number): number {
+    // JS shift operators are 32 bit - so we have to use divide.
+    return Math.floor(timestamp/COUNTER_MASK);
+}
+
+/**
+ * Extract the counter portion of a timestamp.
+ */
+export function extractCounterFromTimestamp(timestamp: number): number {
+    return timestamp & COUNTER_MASK;
+}
+
+/**
+ * Combine a time (in seconds since the start of our locally defined epoch) and a
+ * counter (max size 2^20) into a timestamp.
+ */
+export function makeTimestamp(time: number, counter: number): number {
+    if(time < 0 || !Number.isSafeInteger(time*COUNTER_MASK))
+        throw new Error(`internal error: invalid time component of timestamp ${time}`);
+    if(counter < 0 || !Number.isSafeInteger(counter) || counter >= COUNTER_MASK)
+        throw new Error(`internal error: invalid counter component of timestamp ${counter}`);
+    // We are doing this with multiply and add (instead of <<< and |)
+    // because the JS bit operators are 32 bit only.
+    return time*COUNTER_MASK + counter;
+}
+
+/**
+ * Returns current system time as seconds (UTC) since start our
+ * locally defined epoch.
+ */
+export function currentSystemTimeInLocalEpoch(): number {
+    return Math.floor((+new Date()-LOCAL_EPOCH_START)/1000);
+}
+
+/**
+ * Returns current system time as seconds (UTC) since start our
+ * locally defined epoch.  Time freezes when we hit the rapture time.
+ * (which will allow us another 1 billion events using the multi-
+ * event per second overflow mechanism before the real end times).
+ */
+export function currentSystemTimeInLocalEpochOrRaptureTime(): number {
+    return Math.min(currentSystemTimeInLocalEpoch(), RAPTURE_TIME);
+}
+
